7c7ecff9e6cd4a67264e2444f755d1aa7a1f3908
Carlos <solrak_27@hotmail.com>
2025-06-18 20:10:44 +0200

    Clases DTO creadas para mejorar el diseño y evitar errores con los datos, tengo que terminarlas

---
+++ src/main/java/com/concesionario/ventacar/dto/VehiculoCreateDTO.java
@@ -0,0 +1,53 @@
+package com.concesionario.ventacar.dto;
+
+public class VehiculoCreateDTO {
+
+    private String marca;
+    private String tipo;
+    private Integer precio;
+    private String imagen;
+    private String descripcion;
+
+    public VehiculoCreateDTO() {
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public String getImagen() {
+        return imagen;
+    }
+
+    public void setImagen(String imagen) {
+        this.imagen = imagen;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+}
