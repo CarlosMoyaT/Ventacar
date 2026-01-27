6d7711851b2d09c6e52318a87378366e2eb232de
Carlos <solrak_27@hotmail.com>
2026-01-27 19:03:19 +0100

    need create LoginResponseDTO class for login endpoint and authentication

--- src/main/java/com/concesionario/ventacar/Controller/AuthController.java
+++ src/main/java/com/concesionario/ventacar/Controller/AuthController.java
@@ -7,6 +7,11 @@ import com.concesionario.ventacar.dto.LoginRequestDTO;
 import com.concesionario.ventacar.dto.SignupRequestDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 
@@ -45,12 +50,25 @@ public class AuthController {
         }
     }
 
+    //Crear el LoginResponseDTO
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequestDTO request) {
-        User authenticatedUser = authService.authenticateUser(request.getEmail(), request.getPassword());
-        if (authenticatedUser != null) {
-            return ResponseEntity.ok("Login correcto");
+    public ResponseEntity<LoginResponseDTO> login(@RequestBody LoginRequestDTO request) {
+        try {
+            Authentication authentication = authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            request.getEmail(),
+                            request.getPassword()
+                    )
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+            UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+            String jwt = jwtService.generateToken(userDetails);
+
+            return ResponseEntity.ok(new LoginResponseDTO(jwt, "Bearer"));
+        } catch (AuthenticationException e) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(new LoginResponseDTO(null, "Incorrect login"));
         }
-        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Login incorrecto");
     }
 }
